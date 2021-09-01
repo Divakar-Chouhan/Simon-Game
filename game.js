3df09f57f3c507e2796abef76cd9fe1ed5ca8a81
Divakar Chouhan <87907610+Divakar-Chouhan@users.noreply.github.com>
2021-09-01 15:04:57 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,18 +1,38 @@
+var buttonColours=["red","blue","green","yellow"];
 var gamePattern=[];
 var userClickedPattern=[];
-var buttonColours=["red","blue","green","yellow"];
+//Create a new variable called level and start at level 0.
 var level=0;
-var started;
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started=false;
 
-if(started===false)
-{
-    $(document).on("keydown",nextSequence);
-    started=true;
-}
-alert(started);
+
+$(document).one("keydown",function(){
+
+    if(!started)
+    {
+        $("#level-title").text("level "+level);
+        nextSequence();
+        started=true;
+    }
+    
+});
+    
+// storage of user pattern
+$(".btn").on("click",function(){
+
+    var userChosenColour=this.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
 
 
 function nextSequence(){
+    // Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+    level++;
+    // Inside nextSequence(), update the h1 with this change in the value of level.
+    $("#level-title").text("level "+level);
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColour=buttonColours[randomNumber];
 
@@ -20,22 +40,12 @@ function nextSequence(){
 
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    // audio addition for specific button
-
     playSound(randomChosenColour);
-    level++;
-    $("h1").text("level "+level);
+    
 }
 
-// storage of user pattern
-$(".btn").on("click",handler);
+
    
-function handler(){
-    var userChosenColour=this.id;
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-}
 
 // Function for playing sound
 
